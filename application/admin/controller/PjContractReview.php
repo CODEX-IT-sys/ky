acfe1b8355d9b63113518867eaa292cea59b6914
ky <IT@codex-trans.com>
2021-04-15 17:38:46 +0800

    批量修改项目汇总同步项目描述

--- application/admin/controller/PjContractReview.php
+++ application/admin/controller/PjContractReview.php
@@ -781,23 +781,29 @@ class PjContractReview extends Common
                 'Pre_Format_Delivery_Time','Translation_Delivery_Time',
                 'Revision_Delivery_Time','Post_Format_Delivery_Time'];
 
-            if(in_array($data['field'], $f)){
-                foreach ($Filing_Code as $k=>$v){
-                    Db::name('pj_project_profile')
-                        ->where('Filing_Code', $v['Filing_Code'])
-                        ->update($arr);
+            foreach ($field as $key=>$val){
+                if(in_array($val, $f)){
+                    foreach ($Filing_Code as $k=>$v){
+                        $res=   Db::name('pj_project_profile')
+                            ->where('Filing_Code', $v['Filing_Code'])
+                            ->update($arr);
+                        dump($res);
+                    }
                 }
             }
+
             // 同步更新 项目数据库表 相关信息
             $d = ['Translator','Reviser','Pre_Formatter','Post_Formatter','Language','File_Type','File_Category',
                 'Completed','Delivered_or_Not','File_Category', 'PA'];
-            if(in_array($data['field'], $d)) {
+        foreach ($field as $key=>$val){
+            if(in_array($val, $d)) {
                 foreach ($Filing_Code as $k=>$v){
                     Db::name('pj_project_database')
                         ->where('Filing_Code', $v['Filing_Code'])
                         ->update($arr);
                 }
             }
+        }
 
         } catch (ValidateException $e) {
             // 这是进行验证异常捕获
